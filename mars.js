7a683f0986f89bc03921401d20a51bd5d262ecec
Devarsh <87245954+Shvetangdave@users.noreply.github.com>
2023-03-22 19:48:20 -0400

    Add files via upload

---
+++ mars.js
@@ -0,0 +1,11 @@
+
+function back(){
+    window.location='ear.html';
+   }
+   
+   function next(){
+   window.location='jup.html';
+   }
+   function home(){
+    window.location='inesolar.html';
+    }
